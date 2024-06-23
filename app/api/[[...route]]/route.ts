38c728fb6884f1e9e7995787a2e0b0e1ab4b4761
katsiev1997 <katsiev1997@gmail.com>
2024-06-24 00:18:03 +0300

    Header component

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,19 +1,14 @@
-import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
 
-import authors from "./authors";
-import books from "./books";
-
-import { z } from "zod";
-import { zValidator } from "@hono/zod-validator";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
-app.route("/authors", authors);
-app.route("/books", books);
+app.get("/hello", (c) => {
+  return c.json({ hello: "world" });
+});
 
 export const GET = handle(app);
 export const POST = handle(app);
