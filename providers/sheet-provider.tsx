0da2893684554f8c981048955373cdbbd9d7cd85
katsiev1997 <katsiev1997@gmail.com>
2024-08-06 01:24:59 +0300

    Transactions form

--- providers/sheet-provider.tsx
+++ providers/sheet-provider.tsx
@@ -8,6 +8,8 @@ import { EditAccountSheet } from "@/features/accounts/components/edit-account-sh
 import { NewCategorySheet } from "@/features/categories/components/new-category-sheet";
 import { EditCategorySheet } from "@/features/categories/components/edit-category-sheet";
 
+import { NewTransactionSheet } from "@/features/transactions/components/new-transaction-sheet";
+
 export const SheetProvider = () => {
   const isMounted = useMountedState();
   if (!isMounted) {
@@ -17,8 +19,11 @@ export const SheetProvider = () => {
     <>
       <NewAccountSheet />
       <EditAccountSheet />
+
       <NewCategorySheet />
       <EditCategorySheet />
+      
+      <NewTransactionSheet />
     </>
   );
 };
